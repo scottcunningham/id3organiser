a911b9e0e74557f3f15c9854a9a50a7671041bf7
Scott Cunningham <cunninsc@tcd.ie>
2012-12-16 15:23:57 +0000

    It works! Found how to extract the year from tags (thanks iPython, you're the best), and using shutils to copy files. :)

--- MusicFile.py
+++ MusicFile.py
@@ -7,18 +7,17 @@ class MusicFile:
         self.filepath = filepath
         self.audiofile = eyed3.load(filepath)
         self.addId3Tags()
-    
+
     def addId3Tags(self):
         # For title, artist, album etc we will replace ' ' with '_' etc
-        
         self.trackNo = self.audiofile.tag.track_num[0] # track_num returns a tuple (songNo, songNoOf)
         self.title =  string.replace(self.audiofile.tag.title, ' ', '_')
         self.artist = string.replace(self.audiofile.tag.artist, ' ', '_')
         self.album = string.replace(self.audiofile.tag.album, ' ', '_') 
-        # Broken, no explanation why. :(
-        self.year = self.audiofile.tag.release_date 
-
-        self.format = ".mp3" # ???!
+        self.year = self.audiofile.tag.recording_date
+        
+        self.albumstr = self.album + "_(" + str(self.year) + ")"
+        self.format = self.filepath.split('.')[-1:][0]
 
         return None
 
@@ -29,11 +28,12 @@ class MusicFile:
         if basepath[len(basepath)-1] == '/':
             separator = ''
         else: separator = '/'
-        return basepath + separator + self.artist + "/" + self.album + "_(" + str(self.year) + ")/" + str(self.trackNo) + "_-_" + self.title + self.format
+        return (basepath + separator + self.artist + "/" + self.album + "_(" +
+                str(self.year) + ")/" + str(self.trackNo) + "_-_" + self.title + "." + self.format)
     
     def dump(self):
-        print "Track #: ", self.trackNo
-        print "Title: ", self.title
-        print "Artist: ", self.artist
-        print "Album: ", self.album
-        print "Year: ", self.year
+        print "Track #:", self.trackNo
+        print "Title:", self.title
+        print "Artist:", self.artist
+        print "Album:", self.album
+        print "Year:", self.year
--- Sorter.py
+++ Sorter.py
@@ -1,40 +1,41 @@
 from MusicFile import MusicFile
+import os
+import os.path
+import shutil
 
 def findMusic(dir):
     print "Searching in", dir
 
     files = []
 
-    filenames = ["abc.mp3", "dododo.mp3", "blah.mp3"]
-    #filenames = findAllInCurDir
+    filenames = os.listdir(dir)
     for filename in filenames:
-        print "Adding", filename
-        files.append(MusicFile(filename))    
-
-    if dir == "/home/scott/Music":
-        folders = ["folder1"]
-    #folders = findAllFoldersInCurDir
-    else:
-        folders = []
-
-    for folder in folders:
-        files.appendall(findMusic(dir + folder))
+        if os.path.isdir(dir + "/" + filename):
+            print "Adding folder", filename
+            for file in findMusic(dir + "/" + filename):
+                files.append(file)
+        elif os.path.isfile(dir + "/" + filename):
+            print "Adding", filename
+            try:
+                files.append(MusicFile(dir + "/" + filename))
+            except: 
+                print "Not a music file -", dir + "/" + filename
+        else:
+            print "Not a file or directory:", dir + "/" + filename
 
     return files
 
-basedir = "/home/scott/Music"
+basedir = "/media/windows/Users/Scott/Music/The Cure"
+destdir = "/home/scott/mus"
 
 # Get a list of files
-#musicfiles = findMusic(basedir)
+musicfiles = findMusic(basedir)
 
-mp3 = MusicFile("test.mp3")
-mp3.dump()
-print mp3.getNewPath("/")
-
-'''
 for file in musicfiles:
-    # We'll want to move each file from current dir to dest dir
-    # Just print it for now
     print "Old path: ", file.filepath
-    print "New path: ", file.getNewPath(basedir)
-'''
+    print "New path: ", file.getNewPath(destdir)
+    if not os.path.exists(destdir + '/' + file.artist):
+        os.makedirs(destdir + '/' + file.artist)
+    if not os.path.exists(destdir + '/' + '/' + file.artist + '/' + file.albumstr):
+        os.makedirs(destdir + '/' + file.artist + '/' + file.albumstr)
+    shutil.copy2(file.filepath, file.getNewPath(destdir))
