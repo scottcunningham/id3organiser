f33920197824229a009e6d1683932278d8c2a651
Scott Cunningham <scottcunningham92@gmail.com>
2015-02-16 22:18:36 +0000

    Small style fixes

--- id3organiser.py
+++ id3organiser.py
@@ -4,25 +4,25 @@ import sorter
 
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option("-s", "--source", action="store", type="str", dest="src",
-                  help="Source directory")
-parser.add_option("-d", "--destination", action="store", type="str",
-                  dest="dest", help="Destination directory")
-parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
-                  default=False, help="Don't print status messages to stdout")
-parser.add_option("-x", "--debug", action="store_true", dest="debug",
-                  default=False, help=
-                  "Do a debug run (simulate without copying files)")
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-s", "--source", action="store", type="str", dest="src",
+                      help="Source directory")
+    parser.add_option("-d", "--destination", action="store", type="str",
+                      dest="dest", help="Destination directory")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
+                      default=False, help="Don't print status messages to stdout")
+    parser.add_option("-x", "--debug", action="store_true", dest="debug",
+                      default=False, help="Do a debug run (simulate without copying files)")
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-if not options.src:
-    parser.print_help()
-    sys.exit()
+    if not options.src:
+        parser.print_help()
+        sys.exit()
 
-if not options.dest:
-    parser.print_help()
-    sys.exit()
+    if not options.dest:
+        parser.print_help()
+        sys.exit()
 
-sorter.sort(options.src, options.dest, options.debug, options.quiet)
+    sorter.sort(options.src, options.dest, options.debug, options.quiet)
--- musicfile.py
+++ musicfile.py
@@ -4,7 +4,6 @@ import os.path
 
 
 class MusicFile:
-
     def __init__(self, filepath):
         self.filepath = filepath
         self.audiofile = eyed3.load(filepath)
